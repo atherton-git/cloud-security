524dbd81df539297d6d4e5c9cd0c5c718018fec9
Jack Atherton <jack@MacBook-Air.local>
2022-11-27 00:55:40 +0000

    "Added comments"

--- Azure/azure-policy-scraper.py
+++ Azure/azure-policy-scraper.py
@@ -8,6 +8,7 @@ print("https://github.com/Azure/azure-policy/tree/master/built-in-policies/polic
 service = (input("Specify the service type from the URL above:"))
 service = service.replace(" ","%20")
 
+# Scraping azure git for policies
 github_url = f'https://github.com/Azure/azure-policy/tree/master/built-in-policies/policyDefinitions/{service}'
 response = requests.get(github_url)
 gitlab_table = BeautifulSoup(response.text, 'html.parser')
@@ -17,6 +18,7 @@ filename = [ ]
 for i in jsonfiles:
         filename.append(i.extract().get_text())
 
+# Loop through the policies and parse the raw data
 policy_objects = pd.DataFrame()
 for i in filename:
     ssl._create_default_https_context = ssl._create_unverified_context
@@ -26,7 +28,8 @@ for i in filename:
 # Normalising the "properties" column, concatenating the output with "id" and "name"
 policy_objects = pd.concat([policy_objects[["id","name"]],pd.json_normalize(policy_objects['properties'],max_level=1)],axis=1)
 
-# Removing "Depracated" Policies
+# Removing "depracated" policies
 policy_objects = policy_objects[~policy_objects['metadata.version'].str.endswith('-deprecated')]
 
+# Writing dataframe to csv
 policy_objects.to_csv(f'output_{service}.csv', columns=["metadata.category","name","id","displayName","description","metadata.version"], index=False)
