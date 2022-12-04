c56831bb9b611d30341b3bc47bc82ecc24b0edcc
Jack Atherton <jack@MacBook-Air.local>
2022-12-04 17:04:01 +0000

    Removing service selection, now processes all services by default

--- Azure/azure-policy-scraper.py
+++ Azure/azure-policy-scraper.py
@@ -4,25 +4,35 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
-print("https://github.com/Azure/azure-policy/tree/master/built-in-policies/policyDefinitions")
-service = (input("Specify the service type from the URL above:"))
-service = service.replace(" ","%20")
+# Compile a list of Azure services
+service_list = [ ]
+res = requests.get('https://github.com/Azure/azure-policy/tree/master/built-in-policies/policyDefinitions')    
+soup = BeautifulSoup(res.text, 'html.parser')   
+file = soup.find_all('a',class_="js-navigation-open Link--primary")
+for i in file:
+    service_list.append(i.extract().get_text().replace(" ","%20"))
 
-# Scraping azure git for policies
-github_url = f'https://github.com/Azure/azure-policy/tree/master/built-in-policies/policyDefinitions/{service}'
-response = requests.get(github_url)
-gitlab_table = BeautifulSoup(response.text, 'html.parser')
-jsonfiles = gitlab_table.find_all(title=re.compile("\.json$"))
+# Compile a list of .json URL's for subsequent processing
+href_list = [ ]
+for i in service_list:
+    github_policies = f'https://github.com/Azure/azure-policy/tree/master/built-in-policies/policyDefinitions/{i}'
+    policy_response = requests.get(github_policies)
+    gitlab_table = BeautifulSoup(policy_response.text, 'html.parser')
+    try:
+        jsonfiles = gitlab_table.find_all('a', {'href': re.compile("\.json$")})
+        for i in jsonfiles:
+            href_list.append(i['href'])
+    except Exception:
+        pass
 
-policy_list = [ ]
-for filename in jsonfiles:
-        policy_list.append(filename.extract().get_text())
+# Remove prefix from existing list
+policy_list = [str(i).removeprefix('/Azure/azure-policy/blob/master/built-in-policies/policyDefinitions') for i in href_list]
 
 # Loop through the policies and parse the raw data
 policy_objects = pd.DataFrame()
 for policy in policy_list:
     ssl._create_default_https_context = ssl._create_unverified_context
-    data = pd.read_json(f'https://raw.githubusercontent.com/Azure/azure-policy/master/built-in-policies/policyDefinitions/{service}/{policy}', typ='series')
+    data = pd.read_json(f'https://raw.githubusercontent.com/Azure/azure-policy/master/built-in-policies/policyDefinitions{policy}', typ='series')
     policy_objects=policy_objects.append(data,ignore_index=True)
 
 # Normalising the "properties" column, concatenating the output with "id" and "name"
@@ -35,6 +45,6 @@ policy_objects = policy_objects[~policy_objects['metadata.version'].str.endswith
 policy_objects.columns = [re.sub('^parameters.|^metadata.',"", i) for i in policy_objects.columns]
 
 # Writing dataframe to csv
-policy_objects.to_csv(f'output_{service}.csv', 
+policy_objects.to_csv(f'output.csv', 
 columns=["category","policyType","name","id","displayName","description","mode","effect.allowedValues","effect.defaultValue","version"],
 index=False)
